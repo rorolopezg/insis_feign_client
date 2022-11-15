69691f09e0ad1b71635171cdcf0051dbd3a7b2bc
rorolopezg <51385170+rorolopezg@users.noreply.github.com>
2022-11-15 13:51:42 -0500

    Dependencias maven para junit

--- target/generated-sources/jaxb/com/baeldung/feign/soap/package-info.java
+++ target/generated-sources/jaxb/com/baeldung/feign/soap/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.11.03 a las 01:07:13 PM CLST 
+// Generado el: 2022.11.15 a las 01:49:14 PM EST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.baeldung.com/springbootsoap/feignclient", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
